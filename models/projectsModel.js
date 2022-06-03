9286b90f0d0497309c29e93f9c92f7259906dee8
Austin Flatt <80080997+austindflatt@users.noreply.github.com>
2022-06-03 00:39:02 -0400

    modified projectsModel

--- models/projectsModel.js
+++ models/projectsModel.js
@@ -3,12 +3,11 @@ const mongoose = require('mongoose');
 const ProjectSchema = new mongoose.Schema({
 	title: { type: String, required: true, unique: true },
 	description: { type: String, required: true },
-	techUsed: { type: [String], required: true },
+	techUsed: { type: String, required: true },
 	image: { type: String },
 	githubLink: { type: String, required: true, unique: true },
 	liveLink: { type: String },
 	youtubeId: { type: String },
-	showViewLive: { type: Boolean, default: true },
 	featuredProject: { type: Boolean, default: false }
 }, { timestamps: true })
 
